66a7c1ba4da098cd6bd6a7042114c52e59a3847c
Daniel O'Brien <marmot.daniel@gmail.com>
2015-08-15 06:16:04 +1000

    added probe, changed to mail

--- settemp.sh
+++ settemp.sh
@@ -8,7 +8,8 @@ ALERT_HIGH=380 # 38.0 C
 LOG=/home/pi/temp.log
 PROBE_PATH="/sys/bus/w1/devices/"
 
-PROBE_HEATMAT=$PROBE_PATH/"28-04146f7399ff/w1_slave"
+PROBE_HEATMAT=$PROBE_PATH"28-04146f7399ff/w1_slave"
+PROBE_AMBIENT=$PROBE_PATH"28-04146f6d29ff/w1_slave"
 GPIO_HM=17
 
 #load gpio stuff
@@ -18,9 +19,19 @@ source gpio
 if [ ! -f $PROBE_HEATMAT ]
 then
 	echo [`date '+%c'`]: CRITICAL: "Can't find temp probe!!" >> $LOG
-	sendmail marmot.daniel@gmail.com <<< "Subject: Sneaker - Critical 
-Temp probe missing, last log:
-`tail $LOG`" &
+	 
+	echo "Temp probe missing, last log:
+`tail $LOG`" | mail -aFrom:pi@marmotic.com -s "Sneaker - Critical" marmot.daniel@gmail.com &
+
+	# Failback toggle heat mat
+	if [ `gpio get $GPIO_HM` -eq 0 ]
+	then 
+		gpio mode $GPIO_HM out
+		gpio write $GPIO_HM 1
+	else
+		gpio mode $GPIO_HM out
+		gpio write $GPIO_HM 0
+	fi	
 exit 1
 fi
 
@@ -57,7 +68,9 @@ fi
 if [ $temp -gt $ALERT_HIGH ] || [ $temp -lt $ALERT_LOW ]
 then
 	echo [`date '+%c'`]: CRITICAL: "Temp out of range: $tempFP" >> $LOG
-        sendmail marmot.daniel@gmail.com <<< "Subject: Sneaker - Critical 
-Temp out of range: $tempFP, last log:
-`tail $LOG`" &
+	echo "Temp out of range: $tempFP, last log:
+`tail $LOG`" | mail -aFrom:pi@marmotic.com -s "Subject: Sneaker - Critical" marmot.daniel@gmail.com &
+fi
+
 exit 0
+

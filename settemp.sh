e877d28066aaff02953bf468afde695d423b8043
Daniel O'Brien <marmot.daniel@gmail.com>
2015-09-14 01:35:15 +1000

    significant remake of setsemp

--- settemp.sh
+++ settemp.sh
@@ -7,11 +7,15 @@ ALERT_LOW=300 # 30.0 C
 ALERT_HIGH=400 # 40.0 C
 LOG=/home/pi/temp.log
 PROBE_PATH="/sys/bus/w1/devices/"
+NON=1
+NOFF=0
 
 PROBE_HEATMAT=$PROBE_PATH"28-04146f7399ff/w1_slave"
 PROBE_AMBIENT=$PROBE_PATH"28-04146f6d29ff/w1_slave"
 PROBE_WRMHIDE=$PROBE_PATH"28-04146f5a3eff/w1_slave"
+PROBE_COOLHEAT=$PROBE_PATH"28-04146f755eff/w1_slave"
 GPIO_HM=17
+GPIO_CHM=18
 
 # Get the temp from the tempature file
 function getTemp {
@@ -24,9 +28,43 @@ function getTempFP {
 	echo `bc -l <<< "$tempRAW / 1000" | cut -c1-6`
 }
 
+function setHM {
+# $1 HM temp
+# $2 HM Target max
+# $3 HM Target Min
+# $4 GPIO Pin
+# $5 GPIO N-ON 0/1
+# $6 HM tempFP - floating point temp
+
+## If we are normally off
+if [ "$5" = 0 ]
+then
+	local PIN_OFF=1
+	local PIN_ON=0
+else
+	local PIN_OFF=0
+	local PIN_ON=1
+fi
+
+if [ $1 -gt $2 ] && [ `gpio get $4` -eq $5 ]
+then
+	echo [`date '+%c'`]: Event: Turning off heatmat, HMtemp: $6 >> $LOG
+	gpio mode $4 out
+	gpio write $4 $PIN_OFF #turn off
+
+elif [ $1 -lt $3 ] && [ `gpio get $4` -ne $5 ]
+then
+	echo [`date '+%c'`]: Event: Turning on heatmat, HMtemp: $6 >> $LOG
+	
+	gpio mode $4 out
+	gpio write $4 $PIN_ON #turn on
+fi
+}
+
 #load gpio stuff
 source gpio
 gpio mode $GPIO_HM out
+gpio mode $GPIO_CHM out
 
 #check probe is active
 if [ ! -f $PROBE_HEATMAT ]
@@ -55,31 +93,18 @@ tempHM=`expr $tempRAW / 100`
 
 # Other tank temps
 tempAmbFP=$(getTempFP $PROBE_AMBIENT)
-tempWrmHideFP=`bc -l <<< "$(getTemp $PROBE_WRMHIDE) / 1000" | cut -c1-6`
+tempWrmHideFP=$(getTempFP $PROBE_WRMHIDE)
 
-if [ $tempHM -gt $MAX_TEMP ] && [ `gpio get $GPIO_HM` -eq 0 ]
-then
-	echo [`date '+%c'`]: Event: Turning off heatmat, HMtemp: $tempFP >> $LOG
-	gpio mode $GPIO_HM out
-	gpio write $GPIO_HM 1 #turn off
+setHM $tempHM $MAX_TEMP $MIN_TEMP $GPIO_HM $NOFF $tempFP
 
-else if [ $tempHM -lt $MIN_TEMP ] && [ `gpio get $GPIO_HM` -eq 1 ]
-		then
-		echo [`date '+%c'`]: Event: Turning on heatmat, HMtemp: $tempFP >> $LOG
-		gpio mode $GPIO_HM out
-		gpio write $GPIO_HM 0 #turn on
-
-	else
-		if [ `gpio get $GPIO_HM` -eq 0 ]
-		then 
-			MAT_STATUS=ON
-		else
-			MAT_STATUS=OFF
-		fi
-		echo "[`date '+%c'`] Info: HeatMat $tempFP, heat $MAT_STATUS; WarmHide: $tempWrmHideFP, Ambient: $tempAmbFP" >> $LOG
-	fi
-fi
-
+if [ `gpio get $GPIO_HM` -eq 0 ]
+then 
+	MAT_STATUS=ON
+else
+	MAT_STATUS=OFF
+fi	
+	
+echo "[`date '+%c'`] Info: HeatMat $tempFP, heat $MAT_STATUS; WarmHide: $tempWrmHideFP, Ambient: $tempAmbFP" >> $LOG
 
 # Alert if out of range
 if [ $tempHM -gt $ALERT_HIGH ] || [ $tempHM -lt $ALERT_LOW ]

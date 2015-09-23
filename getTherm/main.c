93d1ccb9f70a3b8fc49409caaed002f729fb599b
daniel <daniel@marmotic.com>
2015-09-24 02:18:26 +1000

    finished getTemp program

--- getTherm/main.c
+++ getTherm/main.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
-#include <fcntl.h>
+#include <fcntl.h> /* open() */
+#include <string.h>
+
+#include <stdlib.h>
+
+#include <errno.h>
 
 #define ARGS 2
+#define FILE_ARG 1
+#define BUFFER_SIZE 100
+#define TEMP_LEN 5
 
 int main (int argc, char * argv[])
 {
 	// tempature file
-	int tfile;
+	int tfile = 0;
+	// buffer for read of file
+	char buffer[BUFFER_SIZE];
+	char * ptmp = NULL;
+	char temp[TEMP_LEN+1];
 
 	if( argc < ARGS)
 	{
@@ -14,8 +26,33 @@ int main (int argc, char * argv[])
 		return 1;
 	}
 
-	tfile = 
+	// Open file + error chk
+	tfile = open(argv[FILE_ARG], O_RDONLY );
+	if (tfile < 0)
+	{
+		fprintf(stderr, "unable to open file\n");
+		return 1;
+	}
 	
+	// Read file in
+	int bytes_read = read(tfile, buffer, BUFFER_SIZE-1);
+	
+	buffer[bytes_read] = '\0';
 
-	return 0;
+	if (strstr( buffer, "YES") == NULL )
+	{
+		fprintf(stderr, "Valid Temp not found\n");
+		return 1;
+	}
+
+	ptmp = strstr(buffer, " t=");
+	strncpy(temp, ptmp+3, TEMP_LEN);
+	temp[TEMP_LEN] = '\0';
+
+	printf("%s", temp);
+	
+	//printf("%i\n%s\n", tfile, strerror(errno));
+	//printf("%s", buffer);
+
+	return 0; //atoi(temp);
 }

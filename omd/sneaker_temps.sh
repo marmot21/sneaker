852b2b036ce55a602382a40d1d9185301310282b
Daniel O'Brien <marmot.daniel@gmail.com>
2015-09-24 21:23:09 +1000

    finished heatmat check

--- omd/sneaker_temps.sh
+++ omd/sneaker_temps.sh
@@ -3,7 +3,13 @@
 ## in /usr/lib/check_mk_agent/local
 
 whm_temp="/sys/bus/w1/devices/28-04146f5905ff/w1_slave"
+chm_temp="/sys/bus/w1/devices/28-04146f755eff/w1_slave"
 
-tempFP=`bc -l <<< "35864 / 1000" | cut -c1-6`
-echo 0 temp_test temp=$tempFP OK - temp 15
+tempRAW=`getTemp $whm_temp`
+tempWFP=`bc -l <<< "$tempRAW / 1000" | cut -c1-6`
+
+tempRAW=`getTemp $chm_temp`
+tempCFP=`bc -l <<< "$tempRAW / 1000" | cut -c1-6`
+
+echo "P HeatMat_temps warm_mat=$tempWFP;31:37;28:39|cool_mat=$tempCFP;21:28;18:35"
 
